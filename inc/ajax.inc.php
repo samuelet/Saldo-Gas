54d7a1ddada5ee65e04673483f82077a43d0b48d
Samuele Tognini <samuele@samuele.netsons.org>
2012-02-15 11:16:18 +0100

    Rimosso libreria inutilizzata

--- inc/ajax.inc.php
+++
@@ -1,9 +0,0 @@
-<?php
-function list_users() {
-  $prefix = FALSE;
-  if ($_GET['filter']) {
-    $prefix = $_SESSION['saldo_prefix'];
-  }
-  $users = get_users(FALSE,$prefix);
-  return drupal_to_js($users);
-}
